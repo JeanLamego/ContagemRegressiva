b098015275259df28237c2c05659de5219563b3f
Jean Lamego <jeanlamego@hotmail.com>
2023-07-19 19:07:35 -0300

    Criando a função de contagem regressiva para o contador

---
+++ main.js
@@ -0,0 +1,22 @@
+const inputEvent = document.getElementById("eventInput");
+const sendBtn = document.getElementById=("sendBtn");
+const stopBtn= document.getElementById=("stopBtn");
+
+
+let interval;
+let days=0;
+let hours=0;
+let minutes=0;
+let seconds=0;
+
+// sendBtn.addEventListener('click',StartCountdown);
+// stopBtn.addEventListener('click',StopCountdown);
+
+function startCountdown(duration, display){
+
+interval = setInterval(n(){
+
+     
+},1000)
+
+}
